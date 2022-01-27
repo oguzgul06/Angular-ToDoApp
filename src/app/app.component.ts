cff5875c3470a78667c242e3c6aaeeccd4fbd667
Oğuz GÜL <ogzgl06@gmail.com>
2022-01-27 20:41:11 +0300

    Build main-structure & create task-form component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'to-do-app';
+  title = 'My Task List';
+  taskList: string[] = ['task 1', 'task 2', 'task 3', 'task 4'];
 }
